a4ce9ea49e4e24803fdb57488b19e902c0e0f2aa
AlekseiMordas <alexey.mordas@gmail.com>
2014-11-21 17:23:41 +0300

    update testng.xml

--- mobile-walmart-tests/src/main/java/com/walmart/tests/AuthorizationTest.java
+++ mobile-walmart-tests/src/main/java/com/walmart/tests/AuthorizationTest.java
@@ -6,23 +6,21 @@ import org.testng.annotations.Test;
 import com.walmart.ui.bo.User;
 import com.walmart.ui.factory.UserFactory;
 
-
-
 public class AuthorizationTest extends BaseTest {
-	
-	@Test(description = "Sign up with email", priority=0)
+
+	@Test(description = "Sign up with email", priority = 0)
 	public void signUpWithEmail() {
 		final User user = UserFactory.getNewUser();
 		Assert.assertTrue(service.isCreatingNewUserSuccessful(user),
 				"Can't create new User with email");
 	}
-	
-	@Test(description = "Login with registered user",priority=1)
+
+	@Test(description = "Login with registered user", priority = 1, enabled = false)
 	private void loginWithRegisteredUser() throws Exception {
 		service.doLogin(UserFactory.getRegistereUser());
 	}
-	
-	@Test(description = "Login with incorrect credentials", priority=2)
+
+	@Test(description = "Login with incorrect credentials", priority = 2, enabled = false)
 	private void loginWithIncorrectUser() throws Exception {
 		service.doLogin(UserFactory.getIncorrectUser());
 	}
